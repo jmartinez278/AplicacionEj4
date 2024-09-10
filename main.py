3debcead7be1ba8e2a4d9c69b265e187b5c21af6
72218637 <72218637@continental.edu.pe>
2024-09-10 14:29:50 -0500

    Inicio

---
+++ main.py
@@ -0,0 +1,9 @@
+class Geometria:
+    def area_cuadrado(self, lado):
+        return lado * lado
+    def area_rectangulo(self, ancho, alto):
+        return ancho * alto
+    def perimetro_cuadrado(self, lado):
+        return 4 * lado
+    def perimetro_rectangulo(self, ancho, alto):
+        return 2 * (ancho + alto)
